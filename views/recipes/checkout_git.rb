16f5fb5f19918c467de113190a1df8502f22b088
Johan Carstens <86116891+johancar@users.noreply.github.com>
2022-06-07 13:35:32 +0200

    Update checkout_git.rb

--- views/recipes/checkout_git.rb
+++ views/recipes/checkout_git.rb
@@ -5,3 +5,14 @@ package "git" do
   )
   retries 2
 end
+
+apt_package("git") do
+  package_name "git-core"
+  action [:install]
+  retries 2
+  retry_delay 2
+  default_guard_interpreter :default
+  declared_type :package
+  cookbook_name "aws_opsworks_custom_cookbooks"
+  recipe_name "checkout_git"
+end
